e33bf69dc3e304182e1465ca1f3d109ceab08537
Pradeep Gowda <pradeep@btbytes.com>
2024-09-19 00:31:31 -0400

    make repo public

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,11 @@
 export default function Home() {
   return (
     <>
-      <h1 className="text-3xl text-center">hello, world</h1>
+      <h1 className="text-3xl text-center">hello, next.js</h1>
       <p>
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Beatae
-        adipisci soluta ducimus doloribus incidunt? Ipsa quam, consectetur
-        laborum officia reprehenderit cupiditate molestias quas, sed ut dolores
-        cum ea, numquam delectus!
+        This page is my experiments with <code>Next.JS</code>
       </p>
+      <p>The code is deployed to Cloudflare</p>
     </>
   );
 }
